28eb7736a7547bb311e8b8bd9f395a4dca35f6d5
Supriya Kashyap <kashyapsupriya61275@gmail.com>
2023-11-25 22:04:16 +0530

    celebrare

---
+++ app/src/main/java/com/example/celebrare/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.celebrare;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.canhub.cropper.CropImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_GALLERY_PERMISSION = 101;
+    private static final int REQUEST_CODE_GALLERY = 103;
+
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button galleryButton = findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(v -> checkGalleryPermissionAndOpen());
+    }
+
+
+    private void checkGalleryPermissionAndOpen() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            openGallery(); // Proceed to open the gallery
+        } else {
+            requestGalleryPermission(); // Request the permission if not granted
+        }
+    }
+
+    private void requestGalleryPermission() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permission needed")
+                .setMessage("Gallery access permission is needed to select images.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                REQUEST_CODE_GALLERY_PERMISSION
+                        );
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_GALLERY_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery(); // Permission granted, proceed to open the gallery
+            } else {
+                Toast.makeText(this, "Gallery permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_CODE_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_GALLERY && data != null) {
+            imageUri = data.getData();
+            if (imageUri != null) {
+                startCrop(imageUri);
+            }
+        }
+    }
+
+    private void startCrop(Uri imageUri) {
+        CropImageView cropImageView = findViewById(R.id.cropImageView); // Replace with your CropImageView ID
+
+        // Assuming 'imageUri' is the Uri of the image you want to set in the CropImageView
+        cropImageView.setImageUriAsync(imageUri);
+        cropImageView.setGuidelines(CropImageView.Guidelines.ON);
+        cropImageView.setMultiTouchEnabled(true);
+    }
+}
+
+
+
